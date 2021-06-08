dda54a57d94b3c030d04c3810a2a21fda5447e56
edufreestyler <edugaraymartinez@hotmail.com>
2021-06-08 21:00:41 +0100

    capitalized constant names

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('.menu-toggle').forEach((item)=>{item.addEventListener("click", (e) => {
     e.preventDefault();
     document.querySelector('.sidebar-wrapper').classList.toggle('active');
-    const burger = document.getElementById('burger');
-    if (burger.classList.contains('fa-bars')) {
-      burger.classList.remove('fa-bars');
-      burger.classList.add('fa-times')
+    const BURGER = document.getElementById('burger');
+    if (BURGER.classList.contains('fa-bars')) {
+      BURGER.classList.remove('fa-bars');
+      BURGER.classList.add('fa-times')
     }
     else {
-      burger.classList.remove('fa-times');
-			burger.classList.add('fa-bars');
+      BURGER.classList.remove('fa-times');
+			BURGER.classList.add('fa-bars');
     }
   });
   })
---
+++ simple_quiz-master/scripts/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let counter = parseInt(document.querySelector('.counter strong').innerHTML);
+  let score = 0;
+  const UPDATECOUNTERS = function () {
+    let element = this.closest('.trivia-question');
+    if (this.classList.contains('is-correct')) {
+      score += 1;
+    } else {
+      element.querySelector('.is-correct').classList.add('actual-answer');
+    }
+    counter += 1;
+    this.closest('.quiz-answers-wrapper').classList.add('clicked');
+    element.classList.add('hiding');
+
+    setTimeout(function () {
+      element.classList.add('hidden');
+      document.getElementById(counter).classList.remove('hidden');
+      document.querySelector('.counter p strong').innerHTML = counter, false;
+    }, 2000);
+    if (counter === 16) {
+      document.querySelector('.quiz-container').classList.add('hidden');
+      document.querySelector('.quiz-score').classList.remove('hidden');
+      document.querySelector('.quiz-score').innerHTML = ('<div class="finalscore"><h2>You got ' + score + ' correct out of 15</p></div>')
+    }
+  };
+  const BUTTONS = document.querySelectorAll('.single-answer');
+  Array.from(BUTTONS).forEach(function (BUTTONS) {
+    BUTTONS.addEventListener('click', UPDATECOUNTERS, false);
+  });
+});
